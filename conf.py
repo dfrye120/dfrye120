dba319372940d459e075d38ca98498c8b637bc50
dfrye120 <dfrye012@gmail.com>
2025-04-21 07:20:02 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 # conf.py — basic Sphinx config
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('docs'))
 
 project = 'OpenAI Education'
 author = 'Dan Frye'
